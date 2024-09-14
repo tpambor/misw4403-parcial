b9ae810f4c5abafb4bee45b9b5fefd3dd0a15b35
Tim Pambor <t.pambor@uniandes.edu.co>
2024-09-14 23:41:05 +0200

    Agregar controller para supermercado

--- src/supermercado/supermercado.module.ts
+++ src/supermercado/supermercado.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupermercadoEntity } from './supermercado.entity';
 import { SupermercadoService } from './supermercado.service';
+import { SupermercadoController } from './supermercado.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SupermercadoEntity])],
   providers: [SupermercadoService],
+  controllers: [SupermercadoController],
 })
 export class SupermercadoModule {}
