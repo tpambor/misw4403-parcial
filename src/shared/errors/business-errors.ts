17e57e1dd4368207487f54e80468bd75d238ade1
Tim Pambor <t.pambor@uniandes.edu.co>
2024-09-14 13:06:22 +0200

    Crear BusinessLogicException

---
+++ src/shared/errors/business-errors.ts
@@ -0,0 +1,9 @@
+export function BusinessLogicException(message: string, type: number) {
+  this.message = message;
+  this.type = type;
+}
+
+export enum BusinessError {
+  NOT_FOUND,
+  PRECONDITION_FAILED,
+}
