3753fc56fb5dd2f8d30673429f8de0cacfef4b45
Tim Pambor <t.pambor@uniandes.edu.co>
2024-09-14 21:32:47 +0200

    Agregar controller para ciudad

---
+++ src/ciudad/ciudad.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { CiudadService } from './ciudad.service';
+import { CiudadDto } from './ciudad.dto';
+import { CiudadEntity } from './ciudad.entity';
+
+@Controller('cities')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class CiudadController {
+  constructor(private readonly ciudadService: CiudadService) {}
+
+  @Get()
+  async findAll() {
+    return await this.ciudadService.findAll();
+  }
+
+  @Get(':ciudadId')
+  async findOne(@Param('ciudadId') ciudadId: string) {
+    return await this.ciudadService.findOne(ciudadId);
+  }
+
+  @Post()
+  async create(@Body() ciudadDto: CiudadDto) {
+    const ciudad: CiudadEntity = plainToInstance(CiudadEntity, ciudadDto);
+    return await this.ciudadService.create(ciudad);
+  }
+
+  @Put(':ciudadId')
+  async update(
+    @Param('ciudadId') ciudadId: string,
+    @Body() ciudadDto: CiudadDto,
+  ) {
+    const ciudad: CiudadEntity = plainToInstance(CiudadEntity, ciudadDto);
+    return await this.ciudadService.update(ciudadId, ciudad);
+  }
+
+  @Delete(':ciudadId')
+  @HttpCode(204)
+  async delete(@Param('ciudadId') ciudadId: string) {
+    return await this.ciudadService.delete(ciudadId);
+  }
+}
